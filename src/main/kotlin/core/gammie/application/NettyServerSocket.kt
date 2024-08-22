90049c3139c93b220a426137d800036516a7e38b
kimJaeHyeon <kmr2644@gmail.com>
2024-08-22 20:09:24 +0900

    chore: logger import 변경

--- src/main/kotlin/core/gammie/application/NettyServerSocket.kt
+++ src/main/kotlin/core/gammie/application/NettyServerSocket.kt
@@ -3,7 +3,6 @@ package core.gammie.application
 import core.gammie.logger
 import io.netty.bootstrap.ServerBootstrap
 import jakarta.annotation.PreDestroy
-import org.hibernate.query.sqm.tree.SqmNode.log
 import org.springframework.stereotype.Component
 import java.net.InetSocketAddress
 
@@ -12,11 +11,11 @@ class NettyServerSocket(
     private val bootstrap: ServerBootstrap,
     private val tcp: InetSocketAddress
 ) {
-    private val logger = logger()
+    private val log = logger()
 
     fun start() {
         try {
-            logger.info("Netty server started")
+            log.info("Netty server started")
             val channelFuture = bootstrap.bind(tcp)
                 .sync()
             channelFuture.channel().closeFuture().sync()
@@ -28,7 +27,7 @@ class NettyServerSocket(
 
     @PreDestroy
     fun stop() {
-        logger.info("Netty server stopped")
+        log.info("Netty server stopped")
     }
 }
 
