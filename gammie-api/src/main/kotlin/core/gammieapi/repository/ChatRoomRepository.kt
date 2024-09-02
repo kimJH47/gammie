2ea62e977f090d40eeeae8032d536c02f27c9c58
kimJaeHyeon <kmr2644@gmail.com>
2024-09-02 22:49:29 +0900

    feat: 조회 api 구현

--- gammie-api/src/main/kotlin/core/gammieapi/repository/ChatRoomRepository.kt
+++ gammie-api/src/main/kotlin/core/gammieapi/repository/ChatRoomRepository.kt
@@ -1,8 +1,16 @@
 package core.gammieapi.repository
 
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface ChatRoomRepository : MongoRepository<ChatRoom, UUID>
+interface ChatRoomRepository : MongoRepository<ChatRoom, UUID> {
+
+    @Query("{ 'id': { '\$gt': ?0 } }")
+    fun findByIdGreaterThan(lastId: UUID, pageable: Pageable): List<ChatRoom>
+
+    fun findAllBy(pageable: Pageable): List<ChatRoom>
+}
