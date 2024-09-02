86a779265879a7e26f489f97b07b4fecb785976b
kimJaeHyeon <kmr2644@gmail.com>
2024-09-02 22:50:39 +0900

    refactor: 채팅방 검증 구현

--- src/main/kotlin/core/gammie/application/ConnectionService.kt
+++ src/main/kotlin/core/gammie/application/ConnectionService.kt
@@ -1,32 +1,45 @@
 package core.gammie.application
 
+import core.gammie.handler.CustomSocketException
+import core.gammie.handler.ErrorCode
 import core.gammie.logger
 import core.gammie.repository.ConnectionRedisRepository
 import io.netty.channel.Channel
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 
 @Service
 class ConnectionService(
-    private val connectionRedisRepository: ConnectionRedisRepository
+    private val connectionRedisRepository: ConnectionRedisRepository,
+    private val chatRoomClient: ChatRoomClient
 ) {
     private val log = logger()
 
     fun connect(channel: Channel, request: ConnectRequest) {
-        //채팅방 id 검증
-        validateChatRoomId(request.roomId)
-        connectionRedisRepository.createConnection(request.roomId, channel.id().asLongText())
-        channel.writeAndFlush(CustomResponse(ResponseType.CONNECTED))
-            .addListener {
-                if (it.isSuccess) {
-                    log.info("Message was successfully sent to the client.")
-                } else {
-                    log.info("Failed to send the message to the client.")
-                    it.cause().printStackTrace()
+        getChatRoomById(request.roomId)
+            .subscribe(
+                {
+                    connectionRedisRepository.createConnection(request.roomId, channel.id().asLongText())
+                    channel.writeAndFlush(CustomResponse(ResponseType.CONNECTED))
+                        .addListener {
+                            if (it.isSuccess) {
+                                log.info("Message was successfully sent to the client. channel id:${channel.id()}")
+                            } else {
+                                log.warn("Failed to send the message to the client. channel id:${channel.id()}, message:${it.cause().message}")
+                            }
+                        }
+                },
+                {
+                    log.error("Failed to connect to the client", it)
+                    throw it
                 }
-            }
+            )
     }
 
-    private fun validateChatRoomId(roomId: String) {
-        //채팅방 id 가 존재하는지 검증
+    private fun getChatRoomById(roomId: String): Mono<ChatRoomResponse> {
+        return chatRoomClient.asyncFindChatRoomById(roomId)
+            .onErrorMap {
+                throw CustomSocketException(ErrorCode.CHAT_ROOM_NOT_FOUND)
+            }
     }
 }
