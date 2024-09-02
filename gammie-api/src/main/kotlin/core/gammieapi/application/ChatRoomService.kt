72f870ce05b2a16ff18ad441d2468c3238deb04e
kimJaeHyeon <kmr2644@gmail.com>
2024-09-02 22:49:52 +0900

    feat: 조회 api 구현

--- gammie-api/src/main/kotlin/core/gammieapi/application/ChatRoomService.kt
+++ gammie-api/src/main/kotlin/core/gammieapi/application/ChatRoomService.kt
@@ -1,6 +1,8 @@
 package core.gammieapi.application
 
 import core.gammieapi.repository.ChatRoomRepository
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -23,5 +25,33 @@ class ChatRoomService(
             chatRoom.imageUrl,
             chatRoom.genres
         )
+    }
+
+    fun findWithLastId(lastId: String): ChatRoomPageResponse {
+        val uuid = UUID.fromString(lastId)
+
+        val chatRooms =
+            chatRoomRepository.findByIdGreaterThan(uuid, getMaxSizePageable())
+                .takeIf { it.isNotEmpty() }
+                ?.map {
+                    ChatRoomResponse(it.id.toString(), it.name, it.description, it.joinCount, it.imageUrl, it.genres)
+                } ?: return ChatRoomPageResponse("", emptyList())
+
+        return ChatRoomPageResponse(chatRooms.last().id, chatRooms)
+    }
+
+    fun findRecent(): ChatRoomPageResponse {
+        val chatRooms = chatRoomRepository.findAllBy(getMaxSizePageable())
+            .takeIf { it.isNotEmpty() }
+            ?.map {
+                ChatRoomResponse(it.id.toString(), it.name, it.description, it.joinCount, it.imageUrl, it.genres)
+            } ?: return ChatRoomPageResponse("", emptyList())
+
+        return ChatRoomPageResponse(chatRooms.last().id, chatRooms)
+
+    }
+
+    private fun getMaxSizePageable(): Pageable {
+        return PageRequest.of(0, 10)
     }
 }
--- gammie-api/src/main/kotlin/core/gammieapi/presentation/http/ChatRoomController.kt
+++ gammie-api/src/main/kotlin/core/gammieapi/presentation/http/ChatRoomController.kt
@@ -1,10 +1,12 @@
 package core.gammieapi.presentation.http
 
+import core.gammieapi.application.ChatRoomPageResponse
 import core.gammieapi.application.ChatRoomResponse
 import core.gammieapi.application.ChatRoomService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,8 +14,18 @@ class ChatRoomController(
     private val chatRoomService: ChatRoomService
 ) {
 
-    @GetMapping("/api/chat-room/{id}")
+    @GetMapping("/api/chat-rooms/{id}")
     fun findChatRoom(@PathVariable("id") chatRoomId: String): ResponseEntity<ChatRoomResponse> {
         return ResponseEntity.ok(chatRoomService.findOne(chatRoomId))
+    }
+
+    @GetMapping("/api/chat-rooms")
+    fun findAllChatRooms(@RequestParam("lastId") lastId: String): ResponseEntity<ChatRoomPageResponse> {
+        return ResponseEntity.ok(chatRoomService.findWithLastId(lastId))
+    }
+
+    @GetMapping("/api/chat-rooms/recent")
+    fun findRecent() : ResponseEntity<ChatRoomPageResponse> {
+        return ResponseEntity.ok(chatRoomService.findRecent())
     }
 }
