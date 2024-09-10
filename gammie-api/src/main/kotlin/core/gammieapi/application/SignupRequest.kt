3e9e8c5a46fc0851a6c779db73043a8b8fdd9456
kimJaeHyeon <kmr2644@gmail.com>
2024-09-10 22:38:14 +0900

    fix: 검증 어노테이션 수정

--- gammie-api/src/main/kotlin/core/gammieapi/application/SignupRequest.kt
+++ gammie-api/src/main/kotlin/core/gammieapi/application/SignupRequest.kt
@@ -1,11 +1,9 @@
 package core.gammieapi.application
 
-import jakarta.validation.constraints.Max
-import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 data class SignupRequest(
-    @field:NotBlank(message = "Name cannot be empty")
-    @field:Max(value = 25L, message = "max name cannot be less than 25")
-    val nickname : String,
+    @field:Size(max = 25, min = 1, message = "max name cannot be less than 25")
+    val nickname: String,
 )
 
