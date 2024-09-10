6559744b76fa3b8cb466692580d54d4b17c9c03e
kimJaeHyeon <kmr2644@gmail.com>
2024-09-10 22:38:01 +0900

    feat: MethodArgumentNotValidException 핸들링 api 구현

--- gammie-api/src/main/kotlin/core/gammieapi/presentation/http/ExceptionHandler.kt
+++ gammie-api/src/main/kotlin/core/gammieapi/presentation/http/ExceptionHandler.kt
@@ -3,6 +3,7 @@ package core.gammieapi.presentation.http
 import core.gammieapi.application.CustomException
 import org.springframework.http.ResponseEntity
 import org.springframework.web.ErrorResponse
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -10,7 +11,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class ExceptionHandler {
 
     @ExceptionHandler(CustomException::class)
-    fun handle(e : CustomException) : ResponseEntity<ErrorResponse> {
+    fun handle(e: CustomException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.badRequest()
+            .build()
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handle(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        println(e.message)
         return ResponseEntity.badRequest()
             .build()
     }
