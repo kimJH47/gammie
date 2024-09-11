8d0d4f849edb25d996e3523b55327224b9843e62
kimJaeHyeon <kmr2644@gmail.com>
2024-09-11 15:22:59 +0900

    feat: SecurityConfig 작성

---
+++ gammie-api/src/main/kotlin/core/gammieapi/auth/config/SecurityConfig.kt
@@ -0,0 +1,38 @@
+package core.gammieapi.auth.config
+
+import core.gammieapi.auth.application.JwtAuthenticationFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+) {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.formLogin {
+            it.disable()
+        }.httpBasic {
+            it.disable()
+        }.csrf {
+            it.disable()
+        }.logout{
+            it.disable()
+        }.authorizeHttpRequests {
+            it.requestMatchers("/api/login", "/api/signup").permitAll()
+            it.requestMatchers("/api/**").authenticated()
+        }.sessionManagement {
+            it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        }.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+
+    }
+}
