9021e7d43bd23d022a0b9dd49bc89b95e994d519
kimJaeHyeon <kmr2644@gmail.com>
2024-09-14 00:30:31 +0900

    refactor: 핸드쉐이크 이전 토큰 인증처리 하게 변경

--- gammie-chat/src/main/kotlin/core/gammiechat/handler/AuthHandler.kt
+++ gammie-chat/src/main/kotlin/core/gammiechat/handler/AuthHandler.kt
@@ -1,20 +1,36 @@
 package core.gammiechat.handler
 
-import core.gammiechat.application.Payload
 import core.gammiechat.application.TokenValidator
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.ChannelHandlerContext
-import io.netty.channel.SimpleChannelInboundHandler
+import io.netty.channel.ChannelInboundHandlerAdapter
+import io.netty.handler.codec.http.HttpRequest
 import org.springframework.stereotype.Component
+import org.springframework.web.util.UriComponentsBuilder
 
 @Component
 @Sharable
 class AuthHandler(
     private val tokenValidator: TokenValidator
-) : SimpleChannelInboundHandler<Payload>() {
+) : ChannelInboundHandlerAdapter() {
 
-    override fun channelRead0(ctx: ChannelHandlerContext, paylaod: Payload) {
-        tokenValidator.validate(paylaod.token)
-        ctx.fireChannelRead(paylaod)
+
+    override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
+        if (msg is HttpRequest) {
+
+            val uri = msg.uri()
+            val component = UriComponentsBuilder.fromUriString(uri)
+                .build()
+
+            val token = component.queryParams.getFirst("token")
+                ?: throw ChatException(ErrorCode.INVALID_TOKEN)
+
+            val payload = tokenValidator.validateAndGetPayload(token)
+            ctx.channel().attr(ConnectionAttributes.ROOM_ID_KEY).set(payload["roomId"].toString())
+            ctx.channel().attr(ConnectionAttributes.USER_ID_KEY).set(payload["userId"].toString())
+        }
+
+        ctx.fireChannelRead(msg)
     }
+
 }
