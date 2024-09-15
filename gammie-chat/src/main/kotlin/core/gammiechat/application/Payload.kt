12e7ebb0ebdca225466ef3b90984f41d7e51eaa7
kimJaeHyeon <kmr2644@gmail.com>
2024-09-15 11:07:23 +0900

    refactor: token 필드삭제

--- gammie-chat/src/main/kotlin/core/gammiechat/application/Payload.kt
+++ gammie-chat/src/main/kotlin/core/gammiechat/application/Payload.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import java.io.Serializable
 
 data class Payload @JsonCreator constructor(
-    val token: String,
     val body: Any = "",
     val command: Command
 ): Serializable
