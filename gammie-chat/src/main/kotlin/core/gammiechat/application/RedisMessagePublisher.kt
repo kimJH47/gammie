315c82a95b861a32fd2eb03d78cf5f8bf5bbd07e
kimJaeHyeon <kmr2644@gmail.com>
2024-09-18 23:06:25 +0900

    refactor: ReactiveRedisTemplate 사용하게 변경 및 로깅추가

--- gammie-chat/src/main/kotlin/core/gammiechat/application/RedisMessagePublisher.kt
+++ gammie-chat/src/main/kotlin/core/gammiechat/application/RedisMessagePublisher.kt
@@ -1,14 +1,20 @@
 package core.gammiechat.application
 
-import org.springframework.data.redis.core.RedisTemplate
+import core.gammiechat.logger
+import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class RedisMessagePublisher(
-    private val redisTemplate: RedisTemplate<String, Any>
+    private val reactiveRedisTemplate: ReactiveRedisTemplate<String, Any>
 ) {
 
+    private val log = logger()
+
     fun publish(topic: String, message: String) {
-        redisTemplate.convertAndSend("chatroom:${topic}", message)
+        reactiveRedisTemplate.convertAndSend(topic, message)
+            .subscribe {
+                log.info("published topic: $topic, message: $message")
+            }
     }
 }
