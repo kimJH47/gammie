667761416f791db815c615c8175cea4267e8c063
kimJaeHyeon <kmr2644@gmail.com>
2024-09-22 23:30:20 +0900

    feat: SUBSCRIPTION_KEY 추가

--- gammie-chat/src/main/kotlin/core/gammiechat/handler/ConnectionAttributes.kt
+++ gammie-chat/src/main/kotlin/core/gammiechat/handler/ConnectionAttributes.kt
@@ -1,6 +1,7 @@
 package core.gammiechat.handler
 
 import io.netty.util.AttributeKey
+import reactor.core.Disposable
 
 
 class ConnectionAttributes {
@@ -8,6 +9,7 @@ class ConnectionAttributes {
     companion object {
         val ROOM_ID_KEY: AttributeKey<String> = AttributeKey.valueOf("roomId")
         val USER_ID_KEY: AttributeKey<String> = AttributeKey.valueOf("userId")
+        val SUBSCRIPTION_KEY: AttributeKey<Disposable> = AttributeKey.valueOf("subscription")
     }
 
 }
