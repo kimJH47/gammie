fd9f9b517ff109a33eb39cb6ddcfd1fea4b8c148
kimJaeHyeon <kmr2644@gmail.com>
2024-09-24 00:17:08 +0900

    feat: USER_NOT_FOUND 에러코드 추가

--- gammie-api/src/main/kotlin/core/gammieapi/application/ErrorCode.kt
+++ gammie-api/src/main/kotlin/core/gammieapi/application/ErrorCode.kt
@@ -3,5 +3,6 @@ package core.gammieapi.application
 enum class ErrorCode(
     val message: String
 ) {
-    CHAT_ROOM_NOT_FOUND("채팅방이 존재하지 않습니다.");
+    CHAT_ROOM_NOT_FOUND("채팅방이 존재하지 않습니다."),
+    USER_NOT_FOUND("유저가 존재하지 않습니다.")
 }
