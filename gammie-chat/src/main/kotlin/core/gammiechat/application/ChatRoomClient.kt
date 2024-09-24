66f98d33d2a26d3954384e3060fcd36a7b72ab7f
kimJaeHyeon <kmr2644@gmail.com>
2024-09-24 21:03:22 +0900

    feat: exitChatRoom 클라이언트 api 구현

--- gammie-chat/src/main/kotlin/core/gammiechat/application/ChatRoomClient.kt
+++ gammie-chat/src/main/kotlin/core/gammiechat/application/ChatRoomClient.kt
@@ -1,7 +1,12 @@
 package core.gammiechat.application
 
+import core.gammiechat.handler.ChatException
+import core.gammiechat.handler.ErrorCode
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Mono
 
 @Component
@@ -14,5 +19,17 @@ class ChatRoomClient(
             .uri("api/chat-rooms/{id}", id)
             .retrieve()
             .bodyToMono(ChatRoomResponse::class.java)
+    }
+
+    fun exitChatRoom(roomId: String, userId: String): Mono<Unit> {
+        return webClient.post()
+            .uri("api/chat-rooms/exit")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(ExitChatRoomRequest(roomId, userId))
+            .retrieve()
+            .onStatus(HttpStatusCode::is4xxClientError) {
+                Mono.error(ChatException(ErrorCode.BAD_REQEUST_CHAT_API))
+            }
+            .bodyToMono<Unit>()
     }
 }
