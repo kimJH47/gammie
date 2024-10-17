f65a159138042e8c5c4a2a7e03503bf17c525ccf
kimJaeHyeon <kmr2644@gmail.com>
2024-10-17 22:05:19 +0900

    feat: 예외 코드 추가

--- gammie-chat/src/main/kotlin/core/gammiechat/exception/ErrorCode.kt
+++ gammie-chat/src/main/kotlin/core/gammiechat/exception/ErrorCode.kt
@@ -5,5 +5,8 @@ enum class ErrorCode(
 ) {
     COMMAND_NOT_FOUND("일치하는 커멘드가 존재하지 않습니다."),
     INVALID_TOKEN("토큰이 유효하지 않습니다."),
-    BAD_REQUEST_CHAT_API("API 서버 응답이 유효하지 않습니다.")
+    BAD_REQUEST_CHAT_API("API 서버 응답이 유효하지 않습니다."),
+    INTERNAL_API_SERVER_ERROR("API 서버에 문제가 발생 했습니다."),
+    FAIL_SEND_MESSAGE("레디스 메시지 발행에 문제가 발생 했습니다.") {
+    }
 }
