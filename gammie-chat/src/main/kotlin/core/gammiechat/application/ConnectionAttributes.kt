4a4ef6201fb6ff8e5e4d1985de1ae69385734a15
kimJaeHyeon <kmr2644@gmail.com>
2024-10-18 20:02:14 +0900

    refactor: 사용하지않는 Attribute 제거

--- gammie-chat/src/main/kotlin/core/gammiechat/application/ConnectionAttributes.kt
+++ gammie-chat/src/main/kotlin/core/gammiechat/application/ConnectionAttributes.kt
@@ -1,15 +1,11 @@
 package core.gammiechat.application
 
 import io.netty.util.AttributeKey
-import reactor.core.Disposable
 
 
 class ConnectionAttributes {
 
     companion object {
-        val ROOM_ID_KEY: AttributeKey<String> = AttributeKey.valueOf("roomId")
-        val USER_ID_KEY: AttributeKey<String> = AttributeKey.valueOf("userId")
-        val SUBSCRIPTION_KEY: AttributeKey<Disposable> = AttributeKey.valueOf("subscription")
         val CHAT_PAYLOAD_KEY: AttributeKey<ChatPayload> = AttributeKey.valueOf("chatPayload")
     }
 
